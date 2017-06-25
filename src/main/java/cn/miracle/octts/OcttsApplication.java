a0a445ed8bfc214a10bd6a6284cc54c7729d1650
prohf <hfhuangfan@gmail.com>
2017-06-25 20:10:33 +0800

    start spring boot

---
+++ src/main/java/cn/miracle/octts/OcttsApplication.java
@@ -0,0 +1,12 @@
+package cn.miracle.octts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcttsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcttsApplication.class, args);
+	}
+}
