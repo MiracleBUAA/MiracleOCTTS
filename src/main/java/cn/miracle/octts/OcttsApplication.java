c22a7947b7ad7eeec53b47bd291e1bcc362a47e6
prohf <hfhuangfan@gmail.com>
2017-06-27 18:11:00 +0800

    修改启动器，添加httpsession

--- src/main/java/cn/miracle/octts/OcttsApplication.java
+++ src/main/java/cn/miracle/octts/OcttsApplication.java
@@ -4,10 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableRedisHttpSession
 @MapperScan("cn.miracle.octts.dao")
 public class OcttsApplication {
 	public static void main(String[] args) {
