494738e43db3d9cdcccccb7f3ec3d63516da669e
prohf <hfhuangfan@gmail.com>
2017-06-27 20:44:41 +0800

    将Dao改为interface

--- src/main/java/cn/miracle/octts/dao/CourseDao.java
+++ src/main/java/cn/miracle/octts/dao/CourseDao.java
@@ -1,8 +1,11 @@
 package cn.miracle.octts.dao;
 
+import cn.miracle.octts.common.base.BaseMapper;
+import cn.miracle.octts.entity.Course;
+
 /**
  * Created by Tony on 2017/6/27.
  */
-public class CourseDao {
+public interface CourseDao extends BaseMapper<Course> {
 
 }
