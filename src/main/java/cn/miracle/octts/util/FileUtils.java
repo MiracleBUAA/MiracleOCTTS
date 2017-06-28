26c17f32f3def4804c2bc07d4af7682c8201ea4e
prohf <hfhuangfan@gmail.com>
2017-06-28 14:52:36 +0800

    完成文件操作方法

--- src/main/java/cn/miracle/octts/util/FileUtils.java
+++ src/main/java/cn/miracle/octts/util/FileUtils.java
@@ -2,10 +2,11 @@ package cn.miracle.octts.util;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +14,17 @@ import java.util.List;
  */
 public class FileUtils {
 
-    private static final String UPLOAD_SAVE_FOLDER = "C:\\Users\\Tony\\Documents\\";
+    private static final String UPLOAD_SAVE_FOLDER = "/Users/hf/tmp/upload/";
+
+    private static final String DOWNLOAD_FILES_FOLDER = "/Users/hf/tmp/download/";
+
+    public static String saveSingleUploadFile(MultipartFile file) throws IOException {
+        byte[] filebytes = file.getBytes();
+        String file_url = UPLOAD_SAVE_FOLDER + file.getOriginalFilename();
+        Path path = Paths.get(file_url);
+        Files.write(path, filebytes);
+        return file_url;
+    }
 
     public static void saveUploadFiles(List<MultipartFile> files) throws IOException {
         for (MultipartFile file : files) {
@@ -23,5 +34,40 @@ public class FileUtils {
             Path path = Paths.get(UPLOAD_SAVE_FOLDER + file.getOriginalFilename());
             Files.write(path, fileBytes);
         }
+    }
+
+    public static List<ByteArrayOutputStream> getDownloadFiles(List<String> fileUrls) throws IOException {
+        List<ByteArrayOutputStream> BOSList = new ArrayList<>();
+        for (String fileurl : fileUrls) {
+            FileInputStream fileInputStream = new FileInputStream(fileurl);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+            ByteArrayOutputStream byteArrayOutputStream  = new ByteArrayOutputStream();
+
+            int c = bufferedInputStream.read();
+            while (c != -1) {
+                byteArrayOutputStream.write(c);
+                c = bufferedInputStream.read();
+            }
+            bufferedInputStream.close();
+            BOSList.add(byteArrayOutputStream);
+        }
+        return BOSList;
+    }
+
+    public static ByteArrayOutputStream getSingleDownloadFile (String fileUrl) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(fileUrl);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+        ByteArrayOutputStream byteArrayOutputStream  = new ByteArrayOutputStream();
+
+        int c = bufferedInputStream.read();
+        while (c != -1) {
+            byteArrayOutputStream.write(c);
+            c = bufferedInputStream.read();
+        }
+        bufferedInputStream.close();
+
+        return byteArrayOutputStream;
     }
 }
