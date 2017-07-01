c32af524f628bab966261db7427f5f08f54a6275
prohf <hfhuangfan@gmail.com>
2017-07-01 20:38:21 +0800

    添加了homework插入方法，修复了findmaxid的bug， 完成了service

--- src/main/java/cn/miracle/octts/dao/HomeworkDao.java
+++ src/main/java/cn/miracle/octts/dao/HomeworkDao.java
@@ -2,6 +2,8 @@ package cn.miracle.octts.dao;
 
 import cn.miracle.octts.common.base.BaseMapper;
 import cn.miracle.octts.entity.Homework;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 /**
@@ -12,4 +14,37 @@ public interface HomeworkDao extends BaseMapper<Homework> {
             "FROM homework " +
             "WHERE homework_id = #{homework_id}")
     Homework findHomeworkById(Integer homework_id);
+
+    @Insert("INSERT INTO homework  (" +
+            "  gmt_create," +
+            "  gmt_modified," +
+            "  uid," +
+            "  homework_id," +
+            "  course_id," +
+            "  teacher_id," +
+            "  homework_score," +
+            "  homework_status," +
+            "  homework_title," +
+            "  homework_message," +
+            "  homework_start_time," +
+            "  homework_end_time," +
+            "  resubmit_limit) " +
+            "VALUES (" +
+            "#{createtime}, " +
+            "#{updatetime}, " +
+            "#{uid}, " +
+            "#{homework_id}, " +
+            "#{course_id}, " +
+            "#{teacher_id}, " +
+            "#{homework_score}, " +
+            "#{homework_title}, " +
+            "#{homework_message}, " +
+            "#{homework_start_time}, " +
+            "#{homework_end_time}, " +
+            "#{resubmit_limit})")
+    @Options(useGeneratedKeys = true, keyColumn = "id")
+    void InsertHomework(Homework homework);
+
+    @Select("SELECT max(homework_id) FROM homework")
+    Integer findMaxHomeworkId();
 }
--- src/main/java/cn/miracle/octts/service/HomeworkService.java
+++ src/main/java/cn/miracle/octts/service/HomeworkService.java
@@ -1,8 +1,11 @@
 package cn.miracle.octts.service;
 
 import cn.miracle.octts.dao.HomeworkDao;
+import cn.miracle.octts.entity.Homework;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * Created by hf on 2017/6/28.
@@ -13,5 +16,15 @@ public class HomeworkService {
     @Autowired
     HomeworkDao homeworkDao;
 
+    public void InsertHomwork(Homework homework) {
+        Date currenttime = new Date();
+        homework.setCreatetime(currenttime);
+        homework.setUpdatetime(currenttime);
 
+    }
+
+    public Integer findMaxHomeworkId() {
+        Integer maxId = homeworkDao.findMaxHomeworkId();
+        return maxId == null ? 1 : maxId;
+    }
 }
