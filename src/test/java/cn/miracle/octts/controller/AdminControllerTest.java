c0d24cfd322162477fd039f01fa388c1a30561c3
prohf <hfhuangfan@gmail.com>
2017-07-01 17:52:46 +0800

    单元测试

--- src/test/java/cn/miracle/octts/controller/AdminControllerTest.java
+++ src/test/java/cn/miracle/octts/controller/AdminControllerTest.java
@@ -5,8 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,6 +31,10 @@ public class AdminControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
 
     @Test
     public void TestCourseInfomationInsert() throws Exception {
@@ -67,6 +79,35 @@ public class AdminControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void TestUploadStudentList() {
+        try {
+            byte[] filebytes;
+            FileInputStream fs = new FileInputStream("/Users/hf/tmp/test/student_list.xls");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fs);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            int c = bufferedInputStream.read();
+            while (c != -1) {
+                byteArrayOutputStream.write(c);
+                c = bufferedInputStream.read();
+            }
+            bufferedInputStream.close();
 
+            filebytes = byteArrayOutputStream.toByteArray();
+
+            MockMultipartFile file = new MockMultipartFile("file", "student_list_test.xls",
+                    "application/x-xls", filebytes);
+
+            MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+            mockMvc.perform(MockMvcRequestBuilders.fileUpload("/admin/student_list")
+                    .file(file)
+                    .param("uid", "T001")
+                    .param("course_id", "1"))
+                    .andExpect(status().is(200));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
