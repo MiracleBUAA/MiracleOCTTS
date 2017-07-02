97f9e71022f73516f3906a771179d1be984ca495
Tony Ye <yezuoxian@foxmail.com>
2017-07-02 15:29:53 +0800

    新增GroupConfirmDao

--- src/main/java/cn/miracle/octts/dao/GroupConfirmDao.java
+++ src/main/java/cn/miracle/octts/dao/GroupConfirmDao.java
@@ -20,4 +20,10 @@ public interface GroupConfirmDao extends BaseMapper<GroupConfirm> {
             "WHERE group_id = #{group_id}")
     @ResultMap("cn.miracle.octts.dao.GroupConfirmDao.GroupConfirmDetail")
     GroupConfirm findGroupConfirmById(Integer group_id);
+
+    @Select("SELECT gmt_create, gmt_modified, uid, group_id, course_id, group_name, group_owner_id, group_score " +
+            "FROM group_confirm " +
+            "WHERE course_id = #{course_id}")
+    @ResultMap("cn.miracle.octts.dao.GroupConfirmDao.GroupConfirmDetail")
+    List<GroupConfirm> findGroupConfirmByCourseId(Integer course_id);
 }
--- src/main/java/cn/miracle/octts/service/GroupConfirmService.java
+++ src/main/java/cn/miracle/octts/service/GroupConfirmService.java
@@ -2,9 +2,13 @@ package cn.miracle.octts.service;
 
 import cn.miracle.octts.dao.GroupConfirmDao;
 import cn.miracle.octts.entity.GroupConfirm;
+import cn.miracle.octts.dao.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -16,11 +20,43 @@ public class GroupConfirmService {
     @Autowired
     private GroupConfirmDao groupConfirmDao;
 
+    @Autowired
+    private StudentDao studentDao;
+
+
     public List<String> findGroupOwner() {
         return groupConfirmDao.findGroupOwner();
     }
 
     public GroupConfirm findGroupConfirmById(Integer group_id) {
         return groupConfirmDao.findGroupConfirmById(group_id);
+    }
+
+    public List<GroupConfirm> findGroupConfirmByCourseId(Integer course_id) {
+        return groupConfirmDao.findGroupConfirmByCourseId(course_id);
+    }
+
+    public HashMap<String, Object> groupConfirm2Json(GroupConfirm group_confirm) {
+        HashMap<String, Object> data = new HashMap<String, Object>();
+
+        data.put("group_id", group_confirm.getGroup_id());
+        data.put("group_name", group_confirm.getGroup_name());
+        data.put("group_owner_name", studentDao.findStudentNameById(group_confirm.getGroup_owner_id()));
+
+
+        return data;
+    }
+
+    public List<HashMap<String, Object>> getGroupConfirmList(Integer course_id) {
+        List<HashMap<String, Object>> group_confirm_list = new ArrayList<HashMap<String, Object>>();
+
+        List<GroupConfirm> group_confirm_result = findGroupConfirmByCourseId(course_id);
+        Iterator<GroupConfirm> group_confirm_iter = group_confirm_result.iterator();
+        while (group_confirm_iter.hasNext()) {
+            HashMap<String, Object> group_confirm = groupConfirm2Json(group_confirm_iter.next());
+            group_confirm_list.add(group_confirm);
+        }
+
+        return group_confirm_list;
     }
 }
